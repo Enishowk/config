9785499f02bae202e2de9d0f8e791d00ae037aa6
Enishowk <github.outskirts347@simplelogin.fr>
2025-10-17 15:49:51 +0200

    update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,5 +5,5 @@ abbr -a nbs 'npm run build && npm run start'
 abbr -a nd npm run dev
 abbr -a ns npm run start
 abbr -a ncu npx npm-check-updates
-abbr -a gl git log --graph --decorate=short --oneline
+abbr -a gl "git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'"
 abbr -a git-clean ~/.scripts/git-clean.sh
