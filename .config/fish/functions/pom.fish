64d584a1bbaa70902cc35a7b8a9ebe0d9b1b6a93
Enishowk <github.outskirts347@simplelogin.fr>
2026-01-29 10:13:14 +0100

    feat(fish): add pom function

---
+++ .config/fish/functions/pom.fish
@@ -0,0 +1,32 @@
+# Use a pomodoro timer with the fish shell.
+# https://gist.github.com/bashbunni/e311f07e100d51a883ab0414b46755fa
+# This requires https://github.com/caarlos0/timer to be installed and https://github.com/charmbrace$
+
+function pom
+    set split $POMO_SPLIT
+    if ! test -n "$split"
+        set split $(gum choose "25/5" "50/10" "all done" --header "Choose a pomodoro split.")
+    end
+
+    switch $split
+       case '25/5'
+           set work "25m"
+           set break "5m"
+       case '50/10'
+           set work "50m"
+           set break "10m"
+       case 'all done'
+           return
+    end
+
+    timer $work && terminal-notifier -message 'Pomodoro'\
+        -title 'Work Timer is up! Take a Break 😊'\
+        -sound Crystal
+
+    gum confirm "Ready for a break?" && \
+    timer $break && terminal-notifier -message 'Pomodoro'\
+        -title 'Break is over! Get back to work 😬'\
+        -sound Crystal \
+    || pom
+
+end
